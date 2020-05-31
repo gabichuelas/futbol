a05256b1e8e4ac2fcc85aaba5286b09f43494b75
Gaby Mendez <gmendez90@gmail.com>
2020-05-31 18:02:09 -0400

    Add ||= to collection definitions in ST

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -13,9 +13,9 @@ class StatTracker
     teams_path = stat_tracker_params[:teams]
     game_teams_path = stat_tracker_params[:game_teams]
 
-    @games = from_csv(games_path, Game)
-    @teams = from_csv(teams_path, Team)
-    @game_teams = from_csv(game_teams_path, GameTeam)
+    @games ||= from_csv(games_path, Game)
+    @teams ||= from_csv(teams_path, Team)
+    @game_teams ||= from_csv(game_teams_path, GameTeam)
   end
 
   def self.from_csv(stat_tracker_params)
